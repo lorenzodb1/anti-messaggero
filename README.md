7d729067f988efdd568ef8abf57655700f31322e
lorenzodb1 <lorenzodb1@hotmail.it>
2019-01-22 13:32:01 -0800

    Version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# anti-messaggero
-A Firefox add-on to quickly remove the overlay that blocks from reading the article.
+# Anti-Messaggero
+A Firefox add-on to automatically remove the overlay that blocks a user from reading an article.
+
+This add-on was built for educational purpose. Please support Il Messaggero (www.ilmessaggero.it) by buying their subscription.
+
+Version: 1.0.0
+- Removes the overlay that covers the article;
+- Removes the video player that automatically appears when an article is opened.
---
+++ antimessaggero.js
@@ -0,0 +1,13 @@
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", unlockArticle);
+} else {
+    unlockArticle();
+}
+
+function unlockArticle() {
+    let layoverElement = document.body.getElementsByClassName("modal");
+    if (layoverElement.length !== 0) layoverElement[0].remove();
+    document.body.style.overflow = "scroll";
+    let videoPlayerElement = document.body.getElementsByClassName("floatingVideo");
+    if (videoPlayerElement.length !== 0) videoPlayerElement[0].remove();
+}
